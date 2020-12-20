a46c23c0cd22b9f536f1540fd6b5c16ad155eddb
DannyStrelok <hmartindaniel@gmail.com>
2020-12-20 18:22:38 +0100

    Nuevo widget animado, un circulo que se rellena porcentualmente

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_painter/src/challenges/cuadrado_animado_screen.dart';
 import 'package:flutter_custom_painter/src/screens/animaciones_screen.dart';
+import 'package:flutter_custom_painter/src/screens/graficas_circulares_screen.dart';
+import 'file:///C:/Workspace/curso_flutter/12_practica_custom_painter/flutter_custom_painter/lib/src/labs/circula_progress_screen.dart';
 import 'package:flutter_custom_painter/src/screens/headers_screen.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      home: CuadradoAnimadoScreen()
+      home: GraficasCircularesScreen()
     );
   }
 }
