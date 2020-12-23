6d7a11a2516a99e5520acb4bf0f63a9931197731
DannyStrelok <hmartindaniel@gmail.com>
2020-12-23 19:26:24 +0100

    Slideshow terminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_painter/src/challenges/cuadrado_animado_screen.dart';
-import 'package:flutter_custom_painter/src/screens/animaciones_screen.dart';
-import 'package:flutter_custom_painter/src/screens/graficas_circulares_screen.dart';
-import 'file:///C:/Workspace/curso_flutter/12_practica_custom_painter/flutter_custom_painter/lib/src/labs/circula_progress_screen.dart';
-import 'package:flutter_custom_painter/src/screens/headers_screen.dart';
 import 'package:flutter_custom_painter/src/screens/slideshow_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Diseños',
+      title: 'Material App',
       home: SlideshowScreen()
     );
   }
--- lib/src/screens/slideshow_screen.dart
+++ lib/src/screens/slideshow_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_painter/src/widgets/slideshow.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideshowScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset('assets/svgs/slide_1.svg')
-      ),
+      body: Column(
+        children: [
+          Expanded( // EL SLIDESHOW NECESITA UN TAMAÑO DEFINIDO PARA FUNCIONAR, YA QUE EL COLUMN TIENE UN ALTO INFINITO
+            child: Slideshow(
+              indicatorPositionTop: false,
+              bulletPrimary: 17,
+              bulletSecondary: 10,
+              colorPrimary: Colors.red,
+              colorSecondary: Colors.green,
+              slides: [
+                SvgPicture.asset('assets/svgs/slide_1.svg'),
+                SvgPicture.asset('assets/svgs/slide_2.svg'),
+                SvgPicture.asset('assets/svgs/slide_3.svg'),
+                SvgPicture.asset('assets/svgs/slide_4.svg'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Slideshow(
+              indicatorPositionTop: false,
+              bulletPrimary: 25,
+              bulletSecondary: 8,
+              slides: [
+                SvgPicture.asset('assets/svgs/slide_1.svg'),
+                SvgPicture.asset('assets/svgs/slide_2.svg'),
+                SvgPicture.asset('assets/svgs/slide_3.svg'),
+                SvgPicture.asset('assets/svgs/slide_4.svg'),
+              ],
+            ),
+          ),
+          
+        ],
+      )
     );
   }
 }
